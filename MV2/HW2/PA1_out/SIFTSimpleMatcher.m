74d7a872c1e2db94180a3fba7c37248cdc4ad04d
IKhaduri <iraklixaduri@gmail.com>
2017-07-29 12:26:26 +0400

    Machine Vision Homework2

---
+++ MV2/HW2/PA1_out/SIFTSimpleMatcher.m
@@ -0,0 +1,57 @@
+function match = SIFTSimpleMatcher(descriptor1, descriptor2, thresh)
+% SIFTSimpleMatcher 
+%   Match one set of SIFT descriptors (descriptor1) to another set of
+%   descriptors (decriptor2). Each descriptor from descriptor1 can at
+%   most be matched to one member of descriptor2, but descriptors from
+%   descriptor2 can be matched more than once.
+%   
+%   Matches are determined as follows:
+%   For each descriptor vector in descriptor1, find the Euclidean distance
+%   between it and each descriptor vector in descriptor2. If the smallest
+%   distance is less than thresh*(the next smallest distance), we say that
+%   the two vectors are a match, and we add the row [d1 index, d2 index] to
+%   the "match" array.
+%   
+% INPUT:
+%   descriptor1: N1 * 128 matrix, each row is a SIFT descriptor.
+%   descriptor2: N2 * 128 matrix, each row is a SIFT descriptor.
+%   thresh: a given threshold of ratio. Typically 0.7
+%
+% OUTPUT:
+%   Match: N * 2 matrix, each row is a match.
+%          For example, Match(k, :) = [i, j] means i-th descriptor in
+%          descriptor1 is matched to j-th descriptor in descriptor2.
+    if ~exist('thresh', 'var'),
+        thresh = 0.7;
+    end
+
+    match = [];
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                                                              %
+%                                YOUR CODE HERE:                               %
+%                                                                              %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+for i=1:size(descriptor1,1)
+    desc1 = descriptor1(i,:);
+    distances = pdist2(desc1,descriptor2);
+    
+    ind1 = find(distances == min(distances));
+    ind1 = ind1(1);
+    min1 = distances(ind1);
+    distances(ind1) = 100000000000;
+    ind2 = distances == min(distances);
+    min2 = distances(ind2);
+    j = ind1;
+    if min1 < thresh * min2
+        match = [match;[i,j]];
+    end 
+end
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                                                              %
+%                                 END YOUR CODE                                %
+%                                                                              %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+end
